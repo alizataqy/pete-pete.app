a115d65a8f58da3e8df94ca3dfa736b5943bd219
alizataqy <farizbushy@gmail.com>
2026-08-14 19:47:37 +0700

    feat: add custom loading states for agenda list and detail pages

---
+++ src/app/agenda/[id]/loading.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import LoadingScreen from "@/components/application/LoadingScreen";
+
+export default function Loading() {
+  return <LoadingScreen title="Sabar ya, ngab!" description="Lagi nyiapin detail plan lo" />;
+}
